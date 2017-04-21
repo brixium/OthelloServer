78d57837414c8c8123fe660bbc40d96c92d90ed2
asolan.lorenzo <asolan.lorenzo@TAVE04.OSDBSESTO.local>
2017-04-21 13:09:53 +0200

    Ciao

--- src/model/Casella.java
+++ src/model/Casella.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
 
-/**
- *
- * @author asolan.lorenzo
- */
 public class Casella {
+    private int x;
+    private int y;
     
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
 }
