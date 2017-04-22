f871a4ba14ba9d5cedb9f3f1047753d00a8dfe02
brevi.marco <brevi.marco@TAVE03.OSDBSESTO.local>
2017-04-22 10:51:48 +0200

    classe partita

--- src/model/Partita.java
+++ src/model/Partita.java
@@ -1,6 +1,16 @@
-
+//classe che inzializza il campo inoltre gestisce i turni dei client
 package model;
-
 public class Partita {
-    
+    //dichiarazione del campo
+    Campo campo;
+    //dichiarazione di una proprietà per gestire il turno
+    boolean turno;
+    //metodo getPartita: utile 
+    public Partita getPartita(){
+        return this;
+    }
+    //metodo che ti assegna il turno
+    public boolean getTurno(){
+        return turno;
+    }
 }
