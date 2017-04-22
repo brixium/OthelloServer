6d0f1f227cdd40169241ffffa964fc9493ce77a7
pisani.edoardo <pisani.edoardo@TAVE02.OSDBSESTO.local>
2017-04-22 10:53:56 +0200

    class pedina

--- src/model/Pedina.java
+++ src/model/Pedina.java
@@ -1,5 +1,9 @@
 package model;
-
 public class Pedina {
-    
+    Casella posizione;
+    boolean colore;//true se è nero folse se è bianco
+    public Pedina(Casella p ,boolean z){
+        posizione=p;
+        colore=z;
+    }
 }
